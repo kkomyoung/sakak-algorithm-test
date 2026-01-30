0e259340debdf473f1f5a84ca51331187e12ff04
kkomyoung <kya754@gmail.com>
2026-01-30 18:19:27 +0900

    feat: 케이스 추가

--- tests/solutions.test.js
+++ tests/solutions.test.js
@@ -1,12 +1,15 @@
 const { solution1 } = require('../src/solutions/solution1');
+const { solution2 } = require('../src/solutions/solution2');
 
 const testCases = [
   { n: 5, expected: 12 },
   { n: 8, expected: 21 },
+  { n: 68, expected: 11 },
 ];
 
 const solutions = [
   { name: 'solution1', fn: solution1 },
+  { name: 'solution2', fn: solution2 },
 ];
 
 describe('개미수열 가운데 두 자리 테스트', () => {
